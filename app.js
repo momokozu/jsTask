dbe0af7b13d599592a08a918e91ec8999a12969a
momokozu <105656589+momokozu@usersnoreply.github.com>
2022-06-20 22:38:32 +0900

    課題提出２回目

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-for (let i= 1; i<= 50; i++){
-  console.log(i);
-  
-  if((i%10)==0){
-    console.log('今'+i+'回ループしました。');
-  }
+let i ;
+for ( i= 1; i<= 50; i++){
 
   if((i%4)==0){
     console.log('4で割り切れる数です。'+ i );
   }
 
+  if(i==10){
+    console.log('今'+i+'回ループしました。');
+  }
+  if(i==20){
+    console.log('今'+i+'回ループしました。');
+  }
+  if(i==30){
+    console.log('今'+i+'回ループしました。');
+  }
+  if(i==40){
+    console.log('今'+i+'回ループしました。');
+  }
+
 }
-console.log('50回カウントが終わりました。');
+alert((i-1)+'回カウントが終わりました。');
